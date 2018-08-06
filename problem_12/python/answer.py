8c553f7e24fc12f3b0f1a972f0eebd6757abe7e1
Jonathan Arndt <jarndt@demandware.com>
2018-08-06 10:01:48 -0600

    faster 12

--- problem_12/python/answer.py
+++ problem_12/python/answer.py
@@ -9,7 +9,7 @@ f+="/../../"
 
 def getMethod(p,m):
     with open(f+p+"/"+m+".py") as source:
-        ff = imp.load_module(m, source, "../../", ('', '', imp.PY_SOURCE))
+        ff = imp.load_module(m, source, "../", ('', '', imp.PY_SOURCE))
     return ff
 
 def getNumDivisors(t):
@@ -32,8 +32,41 @@ def getTriangleNumDivisors(divs=6):
         i+=1
     return t
 
+def primeFactorisationNoD(number,primeList):
+    #https://www.mathblog.dk/triangle-number-with-more-than-500-divisors/
+    nod=1
+    remain=number
+    for i in range(len(primeList)):
+        if primeList[i]**2 > number:
+            return nod*2
+        exp=1
+        while remain % primeList[i] == 0:
+            exp+=1
+            remain/=primeList[i]
+        nod *= exp
+        if remain == 1:
+            return nod
+    return nod
+
+def getFasterTriNumDivs(n=6):
+    #https://www.mathblog.dk/triangle-number-with-more-than-500-divisors/
+    primes=getMethod("problem_3/python","answer").primesfrom2to(n*2)
+    cnt=0
+    i=2
+    dn=2
+    dn1=0
+    while cnt < n:
+        if i % 2 == 0:
+            dn = primeFactorisationNoD(i+1,primes)
+            cnt=dn * dn1
+        else:
+            dn1=primeFactorisationNoD((i+1)/2,primes)
+            cnt=dn*dn1
+        i+=1
+    return i*(i-1)/2
+
 def main():
-    return getTriangleNumDivisors(500)
+    return  getFasterTriNumDivs(500) #getTriangleNumDivisors(500)
 
 def checkAnswer():
     return getMethod("/","tester").getAnswer(12)
